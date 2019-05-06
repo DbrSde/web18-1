9139ad544a2c439d138a704f7427e6db3710796f
Déborah SOUDE <deb.soude@gmail.com>
2019-05-06 14:46:31 +0200

    Donne tous les résultats des multiples de 42 entre -1222328 et 23456 avec un step de 7

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 
 $version = '7';
-$message = '<h1>Je fais du php' . $version . '</h1>'
+$message = '<h1>Je fais du php' . $version . '</h1>';
 echo $message;
 
-define('TVATxPlein', 0.2);
-echo TVATxPlein;
+$result = '<ul>';
+for ($i = -1222328; $i <= 23456; $i +=7) {
+    if ($i % 5 ==0) {
+        $result .= '<li>' . $i . '</li>';
+    }
+}
+
+$result .= '</ul>';
+echo $result;

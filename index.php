b58488c332ecf52137aa92ae8d892f6edba8eab3
Déborah SOUDE <deb.soude@gmail.com>
2019-05-06 15:07:44 +0200

    Mise en place d'une intégration de site internet, préparation des fichiers index, footer, header et content

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
 
-$version = '7';
-$message = '<h1>Je fais du php' . $version . '</h1>';
-echo $message;
-
-$result = '<ul>';
-for ($i = -1222328; $i <= 23456; $i +=7) {
-    if ($i % 5 ==0) {
-        $result .= '<li>' . $i . '</li>';
-    }
-}
-
-$result .= '</ul>';
-echo $result;

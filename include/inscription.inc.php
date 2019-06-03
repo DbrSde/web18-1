c723320b1885ad3e00163223337ef7e16d441af0
Déborah SOUDE <deb.soude@gmail.com>
2019-06-03 15:33:57 +0200

    J'essaie de régler le problème...

--- include/inscription.inc.php
+++ include/inscription.inc.php
@@ -1,39 +1,42 @@
 <?php
 
-//La fonction isset permet de vérifier sur quelque chose existe.
+//La fonction isset permet de vérifier si quelque chose existe.
 if (isset($_POST['validation'])) {
-    //Récupération des éléments du formulaire
+    // Récupération des éléments du formulaire
     $nom = isset($_POST['nom']) ? $_POST['nom'] : "";
     $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : "";
-    $mail = isset($_POST['mail']) ? $_POST['mail'] : "";
+    $email = isset($_POST['email']) ? $_POST['email'] : "";
     $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : "";
 
+// Initialisation d'un tableau vide
     $erreur = array();
 
     //Vérification des champs (remplis ou non)
-    if ($nom == "") array_push($erreur,"Veuillez saisir votre nom");
-    if ($prenom == "") array_push($erreur,"Veuillez saisir votre prenom");
-    if ($mail == "") array_push($erreur,"Veuillez saisir un mail valide");
-    if ($mdp == "") array_push($erreur,"Veuillez saisir un mot de passe");
+    if ($nom == "") array_push($erreur, "Veuillez saisir un nom");
+    if ($prenom == "") array_push($erreur, "Veuillez saisir un prénom");
+    if ($email == "") array_push($erreur, "Veuillez saisir un mail");
+    if ($mdp == "") array_push($erreur, "Veuillez saisir un mot de passe");
 
 
     if (count($erreur) > 0) {
-        $resultat = "<ul>";
-
+        $msgErreur = "<ul>";
         for ($i = 0; $i < count($erreur); $i++) {
-            $resultat .= '<li>' . $erreur[$i] . '</li>';
+            $msgErreur .= "<li>" . $erreur[$i] . "</li>";
         }
-
-        $resultat .= '</ul>';
-        echo $resultat;
+        $msgErreur .= "</ul>";
+        echo $msgErreur;
         require_once 'formInscription.php';
     } else {
-        $checkMail = "SELECT COUNT(*) FROM t_users WHERE USEMAIL='" . $mail . "'";
-        die($checkMail);
+        //préparation de la requête pour compter le nombre d'occurence
+        $checkMail = "SELECT COUNT(*) FROM t_users WHERE  USEMAIL='" . $email . "'";
+        $nombreOccurences = $pdo->query($checkMail)->fetchColumn();
+
+        if ($nombreOccurences == 0) {
+            $mdp = password_hash($mdp, PASSWORD_DEFAULT);
+        } else {
+            echo 'Tu es déjà inscrit';
+        }
     }
-
-
-
 } else {
     require_once 'formInscription.php';
 }

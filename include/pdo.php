488b3d5fc02f4a467230e069ccf68b93a1befb5d
Déborah SOUDE <deb.soude@gmail.com>
2019-06-03 15:38:05 +0200

    Ajoute le PDO

--- include/pdo.php
+++ include/pdo.php
@@ -10,18 +10,16 @@ try {
             //Méthode statique : Le fait de déclarer des propriétés ou des méthodes comme
             // statiques vous permet d'y accéder sans avoir besoin d'instancier la classe. On paramètre donc grace à ça.
 
-            //Definit le jeu de caractères
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAME utf8',
-
-            //La façon dont on récupère les informations (Ici objet)
+            // Définition du jeu de caractères
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            // Façon dont on récupère les jeux d'enregistrement (sous forme d'objet ici)
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-
-            //Gérer les messages d'erreur/ Niveau d'erreur
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+            // Niveau d'erreurs
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
         ));
 }
-//Si l'instanciation de l'objet $pdo ne fonctionne pas, levée d'exception
+// Si l'instanciation de l'objet $pdo ne fonction pas, levée d'exception
 catch (PDOException $e) {
-    echo "Erreur de connexion :" . $e->getMessage();
-
+    // Affichage du message d'erreur
+    echo "Erreur de connexion : " . $e->getMessage();
 }

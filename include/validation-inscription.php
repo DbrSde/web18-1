6f284c9b6e1bc0a76a04dc27ffe8a73e5727b03b
Déborah SOUDE <deb.soude@gmail.com>
2019-06-05 15:02:24 +0200

    Requête SQL de confirmation de validation d'inscription dans le temps

--- include/validation-inscription.php
+++ include/validation-inscription.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: debso
- * Date: 04/06/2019
- * Time: 10:03
- */
+$mail = isset($_GET['mail']) ? $_GET['mail'] : "";
+$token = isset($_GET['token']) ? $_GET['token'] : "";
+$sql = "SELECT * FROM t_users
+        WHERE usemail='$mail'
+        AND usetoken='$token'";
+$result = $pdo->query($sql);
+if ($result->rowCount() == 0) {
+    echo "Dis donc Michel, tu fais quoi avec l'URL ?";
+}
+else {
+    $truc = $result->fetchObject();
+    $timeRegistration = strtotime($truc->usedatetime);
+    $timeNow = strtotime('now');
+    $delta = $timeNow - $timeRegistration;
+    if ($delta <= 3600) {
+        $sql = "UPDATE t_users SET USEACTIF=1 WHERE USEMAIL='$mail'";
+        $bidule = $pdo->prepare($sql);
+        $bidule->execute();
+        echo "<p>Votre inscription est maintenant confirmée.</p>";
+    }
+    else {
+        echo "Trop tard Michel";
+    }
+}

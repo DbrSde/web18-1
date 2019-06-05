d5e942f080c6878be096c6c4741e5a1ce75e416a
Déborah SOUDE <deb.soude@gmail.com>
2019-06-05 15:04:05 +0200

    Ajout des fichiers manquants dans l'index

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
+date_default_timezone_set('Europe/Paris');
 
 require_once './include/pdo.php';
 require_once './functions/getLang.php';
+require_once './include/head.php';
 require_once './include/header.php';
 require_once './include/content.php';
-require_once './include/authentification.inc.php';
-require_once './include/validation-inscription.php';
 require_once './include/footer.php';
-

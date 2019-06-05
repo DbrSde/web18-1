46c04cb5e39ff135ef51fc7c5197a75b1abafee2
Déborah SOUDE <deb.soude@gmail.com>
2019-06-05 15:13:45 +0200

    Correction des chemins

--- include/content.php
+++ include/content.php
@@ -1,15 +1,15 @@
 <?php
 // Récupération des fichiers avec double extension du répertoire /includes
-$content = glob('./includes/*.inc.php');
+$content = glob('./include/*.inc.php');
 
 // Récupération du paramètre d'URL 'page' (superglobale $_GET)
 $page = isset($_GET['page']) ? $_GET['page'] : "";
 
 // Concaténation de la valeur récupérée pour avoir le bon chemin
-$page = './includes/' . $page . '.inc.php';
+$page = './include/' . $page . '.inc.php';
 
 // On vérifie si la chaîne précédente est présente dans le tableau renvoyé par glob()
-$page = in_array($page, $content) ? $page : './includes/home.inc.php';
+$page = in_array($page, $content) ? $page : './include/home.inc.php';
 
 // Inclusion de la page
 require $page;
--- include/header.php
+++ include/header.php
@@ -1,10 +1,10 @@
 <header style="background: #4CAF50; margin-bottom: 2%;">
     <nav style="width: 60%; margin: auto;">
         <ul style="display: inline-flex; list-style: none; width: 100%; justify-content: space-around; margin: 2% 0;">
-            <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="../index.php?page=home" title="Page d'acceuil">Accueil</a></li>
-            <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="../index.php?page=inscription" title="" Inscription>Inscription</a></li>
-            <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="../index.php?page=connexion" title="Connexion">Connexion</a></li>
-s        </ul>
+            <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="index.php?page=home" title="Page d'acceuil">Accueil</a></li>
+            <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="index.php?page=inscription" title="" Inscription>Inscription</a></li>
+            <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="index.php?page=connexion" title="Connexion">Connexion</a></li>
+        </ul>
     </nav>
 </header>
 

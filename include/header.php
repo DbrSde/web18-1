d1418a48b7fe937890f6837afd11d7674bf6e356
Déborah SOUDE <deb.soude@gmail.com>
2019-06-05 15:39:14 +0200

    Réparation du pourquoi mon formulaire s'affichait pas dans l'onglet connexion

--- include/header.php
+++ include/header.php
@@ -1,6 +1,7 @@
 <header style="background: #4CAF50; margin-bottom: 2%;">
     <nav style="width: 60%; margin: auto;">
         <ul style="display: inline-flex; list-style: none; width: 100%; justify-content: space-around; margin: 2% 0;">
+            <!-- Le ?page=qqc vient chercher en fait le nom du fichier .inc.php -->
             <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="index.php?page=home" title="Page d'acceuil">Accueil</a></li>
             <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="index.php?page=inscription" title="Inscription" >Inscription</a></li>
             <li><a style="text-transform: uppercase; color: black; text-decoration: none;" href="index.php?page=connexion" title="Connexion">Connexion</a></li>
